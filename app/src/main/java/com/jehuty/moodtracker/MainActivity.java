67a77d35811b27cfa751d85df7f3c1026eac61b8
Jehuty7 <rayath7@gmail.com>
2018-09-14 09:58:44 +0200

    - History Activity created - Linked Main Activity and History Activity by History Button with a ClickListener();

--- app/src/main/java/com/jehuty/moodtracker/MainActivity.java
+++ app/src/main/java/com/jehuty/moodtracker/MainActivity.java
@@ -1,13 +1,41 @@
 package com.jehuty.moodtracker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageButton mCommentaryButton;
+    private ImageButton mHistoryButton;
+    private ImageView mSmileyHappyDefault;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initButtons();
+
+        //Display History screen when User click on History Button
+        mHistoryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent historyActivityIntent = new Intent (MainActivity.this, HistoryActivity.class);
+                startActivity(historyActivityIntent);
+            }
+        });
     }
+
+
+    private void initButtons(){
+
+    mCommentaryButton = (ImageButton) findViewById(R.id.activity_main_button_commentary);
+    mHistoryButton = (ImageButton) findViewById(R.id.activity_main_button_history);
+    mSmileyHappyDefault = (ImageView) findViewById(R.id.activity_main_smiley_happy_default);
+}
 }
